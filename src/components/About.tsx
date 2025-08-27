bfe83d96cb62750a1343ac8f88c8c6cb97709519
brf153 <153hsb@gmail.com>
2025-08-27 11:22:38 +0530

    make about responsive
    Signed-off-by: brf153 <153hsb@gmail.com>

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,308 +1,89 @@
-import {
-  Box,
-  Chip,
-  Divider,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Rating,
-  Typography,
-} from "@mui/material";
-import Grid from "@mui/material/Grid";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import { featureLists } from "../constants/index";
-import { useGSAP } from "@gsap/react";
-import { ScrollTrigger, SplitText } from "gsap/all";
-import gsap from "gsap";
-import { useRef } from "react";
-
-gsap.registerPlugin(SplitText, ScrollTrigger);
+import gsap from 'gsap';
+import { SplitText} from 'gsap/all'
+import { useGSAP } from '@gsap/react'
+import { Box } from '@mui/material';
 
 const About = () => {
-  const gridRef = useRef<HTMLDivElement>(null);
-
-  useGSAP(() => {
-    const splitText = new SplitText("#about-title", {
-      type: "words",
-    });
-
-    gsap.from(splitText.words, {
-      duration: 0.2,
-      opacity: 0,
-      y: 50,
-      stagger: 0.1,
-      ease: "expo.out",
-      scrollTrigger: {
-        trigger: "#about-title",
-        start: "top bottom",
-        end: "bottom center",
-        scrub: true,
-      },
-    });
-
-    gsap.from(gridRef.current ? Array.from(gridRef.current.children) : [], {
-      duration: 0.2,
-      opacity: 0,
-      stagger: 0.1,
-      ease: "power1.inOut",
-      scrollTrigger: {
-        trigger: gridRef.current,
-        start: "top bottom",
-        end: "80% bottom",
-        scrub: true,
-      },
-    });
-  }, []);
-
-  return (
-    <Box sx={{ minHeight: "100vh", width: "100%" }}>
-      <Box
-        sx={{
-          height: "80%",
-          width: "100%",
-          p: 10,
-          textAlign: "center",
-          display: "flex",
-          flexDirection: "column",
-          gap: 2,
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            width: "100%",
-          }}
-        >
-          <Box
-            sx={{
-              fontSize: 40,
-              fontWeight: "bold",
-              mb: 2,
-              display: "flex",
-              flexDirection: "column",
-              width: "35%",
-              textAlign: "left",
-              gap: 2,
-            }}
-          >
-            <Chip
-              label="Best Cocktails"
-              sx={{ backgroundColor: "white", width: "fit-content" }}
-            />
-            <Typography
-              sx={{
-                fontSize: "6.2vmin",
-                fontFamily: "font-modern-negra",
-                lineHeight: 1.2,
-                fontWeight: "bold",
-              }}
-              id="about-title"
-            >
-              Where every detail matters—from muddle to garnish
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              fontSize: 20,
-              mb: 2,
-              width: "32%",
-              textAlign: "left",
-              gap: 2,
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <Typography fontSize={"18px"}>
-              Every cocktail we serve is a reflection of our obsession with
-              detail — from the first muddle to the final garnish. That care is
-              what turns a simple drink into something truly memorable.
-            </Typography>
-            <Box sx={{ display: "flex", gap: 4, width: "100%" }}>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "left",
-                }}
-              >
-                <Rating
-                  name="read-only"
-                  value={5}
-                  readOnly
-                  sx={{ color: "white" }}
-                />
-                <Typography
-                  sx={{ mt: 1, fontSize: "18px", fontWeight: "bold" }}
-                >
-                  4.5/5
-                </Typography>
-                <Typography>More than +120000 customers</Typography>
-              </Box>
-              <Divider
-                orientation="vertical"
-                flexItem
-                sx={{ borderColor: "#565656" }}
-              />
-              <Box sx={{ width: "20%" }}>
-                <Box
-                  sx={{
-                    position: "relative",
-                    borderRadius: 24,
-                    background:
-                      "linear-gradient(180deg, #313131 0%, #0F0F0F 100%)",
-                    color: "white",
-                    p: 2,
-                    py: 2,
-                    width: "8.5rem",
-                    height: "3rem",
-                  }}
-                >
-                  <img
-                    src="/images/profile1.png"
-                    alt="profile1"
-                    style={{
-                      position: "absolute",
-                      top: "0.5rem",
-                      left: "1rem",
-                      width: "2rem",
-                    }}
-                  />
-                  <img
-                    src="/images/profile2.png"
-                    alt="profile2"
-                    style={{
-                      position: "absolute",
-                      top: "0.5rem",
-                      left: "2.5rem",
-                      width: "2rem",
-                    }}
-                  />
-                  <img
-                    src="/images/profile3.png"
-                    alt="profile3"
-                    style={{
-                      position: "absolute",
-                      top: "0.5rem",
-                      left: "4rem",
-                      width: "2rem",
-                    }}
-                  />
-                  <img
-                    src="/images/profile4.png"
-                    alt="profile4"
-                    style={{
-                      position: "absolute",
-                      top: "0.5rem",
-                      left: "5.5rem",
-                      width: "2rem",
-                    }}
-                  />
-                </Box>
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-        <Box>
-          <Grid
-            ref={gridRef}
-            container
-            rowSpacing={2}
-            columnSpacing={3}
-            sx={{ mt: 2 }}
-          >
-            <Grid size={3}>
-              <img
-                src="/images/abt1.png"
-                alt="abt1"
-                style={{
-                  borderRadius: 16,
-                  objectFit: "cover",
-                  width: "100%",
-                  height: "100%",
-                }}
-              />
-            </Grid>
-            <Grid size={3}>
-              <Box
-                sx={{
-                  display: "flex",
-                  borderRadius: 8,
-                  flexDirection: "column",
-                  gap: 2,
-                  height: "100%",
-                  background:
-                    "linear-gradient(180deg, #313131 0%, #0F0F0F 100%)",
-                  p: 2,
-                }}
-              >
-                <Typography
-                  sx={{ fontFamily: "font-modern-negra", fontSize: "24px" }}
-                >
-                  Crafted to Impress
-                </Typography>
-                <Divider
-                  flexItem
-                  variant="middle"
-                  sx={{ borderColor: "#565656" }}
-                />
-                <List>
-                  {featureLists.map((feature, index) => (
-                    <ListItem key={index} disablePadding>
-                      <ListItemButton disableTouchRipple>
-                        <ListItemIcon>
-                          <CheckCircleIcon sx={{ color: "white" }} />
-                        </ListItemIcon>
-                        <ListItemText primary={feature} />
-                      </ListItemButton>
-                    </ListItem>
-                  ))}
-                </List>
-              </Box>
-            </Grid>
-            <Grid size={6}>
-              <img
-                src="/images/abt2.png"
-                alt="abt2"
-                style={{
-                  borderRadius: 16,
-                  objectFit: "cover",
-                  width: "100%",
-                  height: "100%",
-                }}
-              />
-            </Grid>
-            <Grid size={8}>
-              <img
-                src="/images/abt3.png"
-                alt="abt3"
-                style={{
-                  borderRadius: 16,
-                  objectFit: "cover",
-                  width: "100%",
-                  height: "100%",
-                }}
-              />
-            </Grid>
-            <Grid size={4}>
-              <img
-                src="/images/abt4.png"
-                alt="abt4"
-                style={{
-                  borderRadius: 16,
-                  objectFit: "cover",
-                  width: "100%",
-                  height: "100%",
-                }}
-              />
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-    </Box>
-  );
-};
-
-export default About;
+ useGSAP(() => {
+	const titleSplit = SplitText.create('#about h2', {
+	 type: 'words'
+	})
+	
+	const scrollTimeline = gsap.timeline({
+	 scrollTrigger: {
+		trigger: '#about',
+		start: 'top center'
+	 }
+	})
+	
+	scrollTimeline
+	 .from(titleSplit.words, {
+		opacity: 0, duration: 1, yPercent: 100, ease: 'expo.out', stagger: 0.02
+	})
+	 .from('.top-grid div, .bottom-grid div', {
+		opacity: 0, duration: 1, ease: 'power1.inOut', stagger: 0.04,
+	}, '-=0.5')
+ })
+ 
+ return (
+	<Box id="about">
+	 <Box className="mb-16 md:px-0 px-5">
+		<Box className="content">
+		 <Box className="md:col-span-8">
+			<p className="badge">Best Cocktails</p>
+			<h2>
+			 Where every detail matters <span className="text-white">-</span>
+				from muddle to garnish
+			</h2>
+		 </Box>
+		 
+		 <Box className="sub-content">
+			<p>
+			 Every cocktail we serve is a reflection of our obsession with detail — from the first muddle to the final garnish. That care is what turns a simple drink into something truly memorable.
+			</p>
+			
+			<Box>
+			 <p className="md:text-3xl text-xl font-bold">
+				<span>4.5</span>/5
+			 </p>
+			 <p className="text-sm text-white-100">
+				More than +12000 customers
+			 </p>
+			</Box>
+		 </Box>
+		</Box>
+	 </Box>
+	 
+	 <Box className="top-grid">
+		<Box className="xl:col-span-3">
+		 <Box  className="noisy" />
+		 <img src="/images/abt1.png" alt="grid-img-1" />
+		</Box>
+		
+		<Box className="xl:col-span-6">
+		 <Box  className="noisy" />
+		 <img src="/images/abt2.png" alt="grid-img-2" />
+		</Box>
+		
+		<Box className="xl:col-span-3">
+		 <Box  className="noisy" />
+		 <img src="/images/abt5.png" alt="grid-img-5" />
+		</Box>
+	 </Box>
+	 
+	 <Box className="bottom-grid">
+		<Box className="md:col-span-8">
+		 <Box  className="noisy" />
+		 <img src="/images/abt3.png" alt="grid-img-3" />
+		</Box>
+		
+		<Box className="md:col-span-4">
+		 <Box  className="noisy" />
+		 <img src="/images/abt4.png" alt="grid-img-4" />
+		</Box>
+	 </Box>
+	 
+	</Box>
+ )
+}
+export default About
